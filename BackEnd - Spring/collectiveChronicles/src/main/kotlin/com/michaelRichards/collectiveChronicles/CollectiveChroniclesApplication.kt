b00be5d8752bdb27cc31bed3552066eb236d0895
mnmr1996 <mnmr1996develop@gmail.com>
2024-01-03 15:44:26 -0500

    initializing back end'

---
+++ BackEnd - Spring/collectiveChronicles/src/main/kotlin/com/michaelRichards/collectiveChronicles/CollectiveChroniclesApplication.kt
@@ -0,0 +1,11 @@
+package com.michaelRichards.collectiveChronicles
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CollectiveChroniclesApplication
+
+fun main(args: Array<String>) {
+	runApplication<CollectiveChroniclesApplication>(*args)
+}
