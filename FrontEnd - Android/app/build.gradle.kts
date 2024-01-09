1d5716e5f631ab42ffd0f900c4a52476e34ef08a
mnmr1996 <mnmr1996develop@gmail.com>
2024-01-09 12:16:39 -0500

    adding in necessary dependencies and setting up the basics for dagger

--- FrontEnd - Android/app/build.gradle.kts
+++ FrontEnd - Android/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -66,4 +68,28 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //navController
+    implementation(libs.androidx.navigation.compose)
+
+    //dagger-hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+    kapt(libs.androidx.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    //retrofit
+    implementation(libs.retrofit)
+    implementation(libs.okhttp)
+    implementation(libs.converter.gson)
+
+    //coil
+    implementation(libs.coil.compose)
+
+    //material icons
+    implementation(libs.androidx.material.icons.extended.android)
+}
+
+kapt {
+    correctErrorTypes = true
 }
