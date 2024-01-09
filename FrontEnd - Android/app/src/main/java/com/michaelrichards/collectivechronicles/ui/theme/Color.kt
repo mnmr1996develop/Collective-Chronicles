65d3edc541cf4923584823d0458a701c2d1e1a47
mnmr1996 <mnmr1996develop@gmail.com>
2024-01-09 11:43:05 -0500

    adding theme, fonts, and colors

--- FrontEnd - Android/app/src/main/java/com/michaelrichards/collectivechronicles/ui/theme/Color.kt
+++ FrontEnd - Android/app/src/main/java/com/michaelrichards/collectivechronicles/ui/theme/Color.kt
@@ -2,10 +2,50 @@ package com.michaelrichards.collectivechronicles.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary10 = Color(0xFF091a2a)
+val Primary20 = Color(0xFF113355)
+val Primary30 = Color(0xFF1a4d7f)
+val Primary40 = Color(0xFF2366a9)
+val Primary80 = Color(0xFFaaccee)
+val Primary90 = Color(0xFFd5e6f6)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkenedPrimary10 = Color(0xFF081a2b)
+val DarkenedPrimary20 = Color(0xFF113355)
+val DarkenedPrimary30 = Color(0xFF194d80)
+val DarkenedPrimary40 = Color(0xFF2266aa)
+val DarkenedPrimary80 = Color(0xFFaaccee)
+val DarkenedPrimary90 = Color(0xFFd4e6f7)
+
+val Secondary10 = Color(0xFF181320)
+val Secondary20 = Color(0xFF302640)
+val Secondary30 = Color(0xFF493960)
+val Secondary40 = Color(0xFF614d80)
+val Secondary80 = Color(0xFFc9bfd9)
+val Secondary90 = Color(0xFFe4dfec)
+
+val Tertiary10 = Color(0xFF0d2618)
+val Tertiary20 = Color(0xFF1a4c30)
+val Tertiary30 = Color(0xFF277248)
+val Tertiary40 = Color(0xFF34985f)
+val Tertiary80 = Color(0xFFb3e5c9)
+val Tertiary90 = Color(0xFFd9f2e4)
+
+val Red10 = Color(0xFF330000)
+val Red20 = Color(0xFF660000)
+val Red30 = Color(0xFF990000)
+val Red40 = Color(0xFFcc0000)
+val Red80 = Color(0xFFff9999)
+val Red90 = Color(0xFFffcccc)
+
+
+val Grey10 = Color(0xFF191C1D)
+val Grey20 = Color(0xFF2D3132)
+val Grey90 = Color(0xFFE0E3E3)
+val Grey95 = Color(0xFFEFF1F1)
+val Grey99 = Color(0xFFFBFDFD)
+
+val PrimaryGrey30 = Color(0xFF364d63)
+val PrimaryGrey50 = Color(0xFF5b81a4)
+val PrimaryGrey60 = Color(0xFF7b9ab7)
+val PrimaryGrey80 = Color(0xFFbdccdb)
+val PrimaryGrey90 = Color(0xFFdee6ed)
--- FrontEnd - Android/app/src/main/java/com/michaelrichards/collectivechronicles/ui/theme/Theme.kt
+++ FrontEnd - Android/app/src/main/java/com/michaelrichards/collectivechronicles/ui/theme/Theme.kt
@@ -10,31 +10,68 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Primary80,
+    onPrimary = Primary20,
+    primaryContainer = Primary30,
+    onPrimaryContainer = Primary90,
+    inversePrimary = Primary40,
+    secondary = DarkenedPrimary80,
+    onSecondary = DarkenedPrimary20,
+    secondaryContainer = DarkenedPrimary30,
+    onSecondaryContainer = DarkenedPrimary90,
+    tertiary = Secondary80,
+    onTertiary = Secondary20,
+    tertiaryContainer = Secondary30,
+    onTertiaryContainer = Secondary90,
+    error = Red80,
+    onError = Red20,
+    onErrorContainer = Red90,
+    errorContainer = Red30,
+    background = Grey10,
+    onBackground = Grey90,
+    surface = PrimaryGrey30,
+    onSurface = PrimaryGrey80,
+    inverseSurface = Grey90,
+    inverseOnSurface = Grey10,
+    surfaceVariant = PrimaryGrey30,
+    onSurfaceVariant = PrimaryGrey80,
+    outline = PrimaryGrey80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = Primary40,
     onPrimary = Color.White,
+    primaryContainer = Primary90,
+    onPrimaryContainer = Primary10,
+    inversePrimary = Primary80,
+    secondary = DarkenedPrimary80,
     onSecondary = Color.White,
+    secondaryContainer = DarkenedPrimary90,
+    onSecondaryContainer = DarkenedPrimary10,
+    tertiary = Secondary40,
     onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    tertiaryContainer = Secondary90,
+    onTertiaryContainer = Secondary10,
+    error = Red40,
+    onError = Color.White,
+    errorContainer = Red90,
+    onErrorContainer = Red10,
+    background = Grey99,
+    onBackground = Grey10,
+    surface = PrimaryGrey90,
+    onSurface = PrimaryGrey30,
+    inverseSurface = Grey20,
+    inverseOnSurface = Grey95,
+    surfaceVariant = PrimaryGrey90,
+    onSurfaceVariant = PrimaryGrey30,
+    outline = PrimaryGrey50
 )
 
 @Composable
@@ -45,10 +82,10 @@ fun CollectiveChroniclesTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+       /* dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+        }*/
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
